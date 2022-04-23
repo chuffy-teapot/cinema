b7580bcb70fdce2bfd134a8076a777830f8e99b2
chuffy-teapot <cvjjgvxqpzm@gmail.com>
2022-04-24 00:10:31 +0300

    js upd

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
-// let menu = document.querySelector(".menu__list");
+let popup = document.querySelectorAll(".popup");
+let button = document.querySelectorAll(".pop__button");
 
-// menu.addEventListener("click", () => {
-// menu.setAttribute("href", "https://html5css.ru/js/js_htmldom_html.php")
-// })
+for (let index = 0; index < button.length; index++) {
+  let buttonNum = button[index];
+  buttonNum.addEventListener("click", openPop);
+
+  let popupNum = popup[index];
+
+  function openPop() {
+    // popupNum = popup[index]; 
+    popupNum.style.display = "Block";
+  }
+
+  window.addEventListener("click", closePop);
+
+  function closePop(e) {
+    if (e.target == popupNum) {
+      popupNum.style.display = "none";
+    }
+  }
+}
+
+
+
+let pick = document.querySelectorAll(".picker__item");
+
+let gen = Math.floor(Math.random() * (80 - 0) + 0);
+
+for (let i = 0; i <= gen; i++) {
+  pick[i].style.opacity = ".5";
+  pick[8].style.opacity = "1";
+  pick[9].style.opacity = "1";
+  pick[4].style.opacity = "1";
+  pick[15].style.opacity = "1";
+  pick[30].style.opacity = "1";
+  pick[66].style.opacity = "1";
+}
