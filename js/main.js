262809755598b5a7694db9a51db93e2effb7ac78
chuffy <98713993+chuffy-teapot@users.noreply.github.com>
2022-05-18 12:04:07 +0500

    theme switcher add

--- js/main.js
+++ js/main.js
@@ -1,5 +1,126 @@
 let popup = document.querySelectorAll(".popup");
 let button = document.querySelectorAll(".pop__button");
+let slideBtn = document.querySelector(".top__btn");
+let slider = document.querySelector(".slider");
+let day = document.querySelector(".day")
+let night = document.querySelector(".night")
+let a;
+
+
+function colorChange(a) {
+
+  if (a !== "night") {
+    document.body.style.backgroundColor = "white"
+    document.querySelector(".menu__section").style.borderRight = "1px solid black"
+    let logo = document.querySelector(".logo");
+    logo.style.color = "#434d57";
+    logo.onmouseenter = () => {
+      logo.style.color = "black"
+    }
+    logo.onmouseleave = () => {
+      logo.style.color = "#434d57"
+    }
+
+    document.querySelector(".form__search").style.backgroundColor = "#99e4e4"
+
+    let items = document.querySelectorAll(".menu__item");
+
+    for (let item of items) {
+      item.onmouseenter = () => {
+        item.style.backgroundColor = "#99e4e4";
+      }
+      item.onmouseleave = () => {
+        item.style.backgroundColor = "";
+      }
+    }
+
+    let tabletRate = document.querySelectorAll(".tablet__rating");
+
+    for (let rating of tabletRate) {
+      rating.style.color = "black";
+    }
+
+    let heading = document.querySelector(".top__text");
+    heading.onmouseenter = () => {
+      heading.style.color = "#434d57";
+    }
+
+    heading.onmouseleave = () => {
+      heading.style.color = "";
+    }
+  }
+  else {
+    document.body.style.backgroundColor = ""
+    document.querySelector(".menu__section").style.borderRight = ""
+    let logo = document.querySelector(".logo");
+    logo.style.color = "";
+    logo.onmouseenter = () => {
+      logo.style.color = ""
+    }
+    logo.onmouseleave = () => {
+      logo.style.color = ""
+    }
+
+    document.querySelector(".form__search").style.backgroundColor = ""
+
+    let items = document.querySelectorAll(".menu__item");
+
+    for (let item of items) {
+      item.onmouseenter = () => {
+        item.style.backgroundColor = "";
+      }
+      item.onmouseleave = () => {
+        item.style.backgroundColor = "";
+      }
+    }
+
+    let tabletRate = document.querySelectorAll(".tablet__rating");
+
+    for (let rating of tabletRate) {
+      rating.style.color = "";
+    }
+
+    let heading = document.querySelector(".top__text");
+    heading.onmouseenter = () => {
+      heading.style.color = "";
+    }
+
+    heading.onmouseleave = () => {
+      heading.style.color = "";
+    }
+  }
+}
+
+day.addEventListener("click", () => {
+  slider.style.left = "28px"
+  day.style.fontSize = "20px"
+  day.style.color = "black"
+  night.style.color = "#E0FFFF"
+  night.style.fontSize = "16px"
+
+  slideBtn.style.backgroundColor = "#434d57"
+  slider.style.backgroundColor = "#E0FFFF"
+
+  a = "day"
+
+  colorChange(a)
+})
+
+night.addEventListener("click", () => {
+  slider.style.left = ""
+  day.style.fontSize = ""
+  night.style.fontSize = ""
+  day.style.color = ""
+  night.style.color = ""
+
+  slideBtn.style.backgroundColor = "";
+  slider.style.backgroundColor = "";
+
+  a = "night";
+
+  colorChange(a)
+})
+
 
 for (let index = 0; index < button.length; index++) {
   let buttonNum = button[index];
@@ -19,47 +140,4 @@ for (let index = 0; index < button.length; index++) {
       popupNum.style.display = "none";
     }
   }
-}
-
-
-let pick = document.querySelectorAll(".picker__item");
-
-let gen = Math.floor(Math.random() * (pick.length - 0) + 0);
-
-for (let i = 0; i <= gen; i++) {
-  pick[i].style.opacity = ".5";
-  pick[8].style.opacity = "1";
-  pick[9].style.opacity = "1";
-  pick[4].style.opacity = "1";
-  pick[15].style.opacity = "1";
-  pick[30].style.opacity = "1";
-  pick[66].style.opacity = "1";
-
-  pick[108].style.opacity = "1";
-  pick[129].style.opacity = "1";
-  pick[134].style.opacity = "1";
-  pick[155].style.opacity = "1";
-  pick[160].style.opacity = "1";
-  pick[196].style.opacity = "1";
-
-  pick[208].style.opacity = "1";
-  pick[229].style.opacity = "1";
-  pick[254].style.opacity = "1";
-  pick[275].style.opacity = "1";
-  pick[290].style.opacity = "1";
-  pick[301].style.opacity = "1";
-
-  pick[318].style.opacity = "1";
-  pick[329].style.opacity = "1";
-  pick[354].style.opacity = "1";
-  pick[370].style.opacity = "1";
-  pick[390].style.opacity = "1";
-  pick[406].style.opacity = "1";
-
-  pick[420].style.opacity = "1";
-  pick[440].style.opacity = "1";
-  pick[460].style.opacity = "1";
-  pick[476].style.opacity = "1";
-  pick[4].style.opacity = "1";
-  pick[15].style.opacity = "1";
 }
